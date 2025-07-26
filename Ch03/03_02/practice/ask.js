7e93b3a283480bc4cbbc0c1c22a75d2eeffea12e
yukixoma08@gmail.com <yukixoma08@gmail.com>
2025-07-26 13:38:27 +0900

    Collecting information with readline

---
+++ Ch03/03_02/practice/ask.js
@@ -0,0 +1,9 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What's your name? > ", (answer) => {
+  console.log(`Hello ${answer}!`);
+});
